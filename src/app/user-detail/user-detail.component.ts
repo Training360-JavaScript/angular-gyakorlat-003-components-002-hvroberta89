fcc0e75620e617de2fcf5b4254f924cbe03901fb
hvroberta89 <veber.roberta@gmail.com>
2022-01-17 22:15:20 +0100

    corrected user-detail modul

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -14,7 +14,7 @@ export class UserDetailComponent implements OnInit {
    * @var user {User} - Input tulajdonság
    * @default új User
    */
-  @Input() user!: User;
+  @Input() user: User = new User;
 
   @Output() delUser: EventEmitter<User> = new EventEmitter();
 
@@ -32,7 +32,7 @@ export class UserDetailComponent implements OnInit {
    * @returns {void}
    */
 
-   onDelete(): void {
+   onDelete(user:User): void {
      this.delUser.emit(this.user);
    }
 
